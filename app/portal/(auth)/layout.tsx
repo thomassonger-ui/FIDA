5065cbecea191a63ab686e9c0c222385af30d8ab
Scout <thomas.songer@gmail.com>
2026-05-16 19:24:56 -0400

    feat(portal): mount AtticusCopyrightBar in auth portal layout

--- app/portal/(auth)/layout.tsx
+++ app/portal/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import { PortalSidebar } from "../Sidebar";
+import { AtticusCopyrightBar } from "@/components/AtticusCopyrightBar";
 
 export default function PortalAuthLayout({
   children,
@@ -9,7 +10,10 @@ export default function PortalAuthLayout({
     <div className="min-h-screen bg-paper flex flex-col">
       <div className="flex flex-1">
         <PortalSidebar />
-        <div className="flex-1 px-8 md:px-12 py-10">{children}</div>
+        <div className="flex-1 flex flex-col">
+          <div className="flex-1 px-8 md:px-12 py-10">{children}</div>
+          <AtticusCopyrightBar />
+        </div>
       </div>
     </div>
   );
